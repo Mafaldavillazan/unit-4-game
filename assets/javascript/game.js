f8d2bef7133fc0591a9c7ebc78860de918c556ac
hunting <mafalda.villazan@gmail.com>
2019-06-18 19:25:10 -0700

    Creating a function that adds the value of each pressed gem to the current score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,12 @@
 $(document).ready(function () {
 
     //Variable that shows the number the user will have and will need to add to = the random number
-    var userNumber = 0
+    var userNumberStart = 1
+    var userNumberAdding = 1
     //add this value to the HTML
-    $("#userNumbertxt").text(userNumber);
+    $("#userNumbertxt").text(userNumberStart);
+
+    //QUESTION: can i create an if statment with on click "if a click the valu is, if not"
 
 
     //Generate a random number between 19-120 for randomNumber 
@@ -16,30 +19,71 @@ $(document).ready(function () {
     }
 
     //Create function that give us random number between two numbers
-    function createRandomNumber(x,y) {
+    function createRandomNumber(min, max) {
         var randomNumber = "";
-        for (var i = 0; i < y; i++) {
-            randomNumber = getRandomNumber(x,y);
+        for (var i = 0; i < max; i++) {
+            randomNumber = getRandomNumber(min, max);
         }
         return randomNumber;
     }
 
-    //Refresh this whenever the page starts o the user has won or lose
-    var currentRandomNumber = createRandomNumber(19,20)
+    //Create a random number between 19-20 for the user to try to reach it
+    //TO.DO: Refresh this whenever the page starts or the user has won or lose
+    var currentRandomNumber = createRandomNumber(19, 120);
     //Print this result on HTML
     $("#randomNumbertxt").text(currentRandomNumber);
 
-    //Generate a random number between for 1-12 randomNumberGem createRandomNumberGem
-    var pinkRandomNumber = createRandomNumber(1,12)
-    var purpleRandomNumber = createRandomNumber(1,12)
-    var blueRandomNumber = createRandomNumber(1,12)
-    var yellowRandomNumber = createRandomNumber(1,12)
-    //Assigns that value to 
-    //we run randomNumberGem to all this variables
-    //pinkRandomNumber 
-    //purpleRandomNumber
-    //blueRandomNumber
-    //yellowRandomNumber
+    //Generate a random number between for 1-12 for each Gem
+    var pinkRandomNumber = createRandomNumber(1, 12)
+    console.log("-----");
+    console.log("Pink Value "+ pinkRandomNumber);
+    var purpleRandomNumber = createRandomNumber(1, 12)
+    console.log("-----");
+    console.log("Purple Value "+ purpleRandomNumber);
+    var blueRandomNumber = createRandomNumber(1, 12)
+    console.log("-----");
+    console.log("Blue Value "+ blueRandomNumber);
+    var yellowRandomNumber = createRandomNumber(1, 12)
+    console.log("-----");
+    console.log("Yellow value "+ yellowRandomNumber);
+
+    // Create .click functions for each gem
+    // When you click on it you activate a function addingNumber(pinkRandomNumber){}
+    //takes the value of that random number and add it on to the userNumber
+    //that value to userNumber + numberToAdd
+    function addingNumbers(gemValue){
+        newValue = userNumberAdding + gemValue
+        return newValue;        
+    }
+
+    $("#pinkGem").click(function () {
+        //We change the user value so it can be add to as a new value with the + of the last interaction
+        userNumberAdding = addingNumbers(pinkRandomNumber);
+        console.log("-----");
+        console.log("New Value  1 + " + pinkRandomNumber + "=" + userNumberAdding);
+    })
+    $("#purpleGem").click(function () {
+        //We change the user value so it can be add to as a new value with the + of the last interaction
+        userNumberAdding = addingNumbers(purpleRandomNumber);
+        console.log("-----");
+        console.log("New Value  1 + " + purpleRandomNumber + "=" + userNumberAdding);
+    })
+    $("#blueGem").click(function () {
+        //We change the user value so it can be add to as a new value with the + of the last interaction
+        userNumberAdding = addingNumbers(blueRandomNumber);
+        console.log("-----");
+        console.log("New Value  1 + " + blueRandomNumber + "=" + userNumberAdding);
+    })
+    $("#yellowGem").click(function () {
+        //We change the user value so it can be add to as a new value with the + of the last interaction
+        userNumberAdding = addingNumbers(yellowRandomNumber);
+        console.log("-----");
+        console.log("New Value  1 + " + yellowRandomNumber + "=" + userNumberAdding);
+    })
+
+    $("#userNumbertxt").text(userNumberAdding);
+
+
 
 
 
@@ -51,18 +95,6 @@ $(document).ready(function () {
 
 
 
-//Generate a random number between for 1-12 randomNumberGem createRandomNumberGem
-    //Assigns that value to 
-    //we run randomNumberGem to all this variables
-    //pinkRandomNumber 
-    //purpleRandomNumber
-    //blueRandomNumber
-    //yellowRandomNumber
-
-// Create .click functions for each gem
-// When you click on it you activate a function addingNumber(pinkRandomNumber){}
-//takes the value of that random number and add it on to the userNumber
-//that value to userNumber + numberToAdd
 
 //Create IF statements for wining and losing
 // we can create a script for this part
