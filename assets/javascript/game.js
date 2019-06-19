7094d210ee2e9be834d62251b21dc140cebcb7fe
hunting <mafalda.villazan@gmail.com>
2019-06-18 18:47:16 -0700

    Adding functions that create random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,44 @@
 //the game starts with a function that has already been load that runs both random functions
 $(document).ready(function () {
 
-    //create variable that could help us later compare
     //Variable that shows the number the user will have and will need to add to = the random number
     var userNumber = 0
     //add this value to the HTML
-    $("#userNumbertxt").append(userNumber);
+    $("#userNumbertxt").text(userNumber);
+
+
+    //Generate a random number between 19-120 for randomNumber 
+    //Create a function that creates random numbers
+    function getRandomNumber(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    //Create function that give us random number that the user will need to reach
+    function createRandomNumber() {
+        var randomNumber = "";
+        for (var i = 0; i < 120; i++) {
+            randomNumber = getRandomNumber(19, 120);
+        }
+        return randomNumber;
+    }
+
+    //Refresh this whenever the page starts o the user has won or lose
+    //Print this result on HTML
+    var currentRandomNumber = createRandomNumber()
+    $("#randomNumbertxt").text(currentRandomNumber);
+
+
+    //This result has to be printed on HTML
+    //Result has to be stored as the randomNumber
+
 
 });
 
 
 
 
-//Generate a random number between 19-120 for randomNumber createRandomNumber
-    //This result has to be printed on HTML
-    //Result has to be stored as the randomNumber
 
 //Generate a random number between for 1-12 randomNumberGem createRandomNumberGem
     //Assigns that value to 
