4c681cfafb4609738b86bd4ad965921f3359da56
hunting <mafalda.villazan@gmail.com>
2019-06-18 18:32:29 -0700

    Adding the score number variable

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+//the game starts with a function that has already been load that runs both random functions
+$(document).ready(function () {
+
+    //create variable that could help us later compare
+    //Variable that shows the number the user will have and will need to add to = the random number
+    var userNumber = 0
+    //add this value to the HTML
+    $("#userNumbertxt").append(userNumber);
+
+});
+
+
+
+
+//Generate a random number between 19-120 for randomNumber createRandomNumber
+    //This result has to be printed on HTML
+    //Result has to be stored as the randomNumber
+
+//Generate a random number between for 1-12 randomNumberGem createRandomNumberGem
+    //Assigns that value to 
+    //we run randomNumberGem to all this variables
+    //pinkRandomNumber 
+    //purpleRandomNumber
+    //blueRandomNumber
+    //yellowRandomNumber
+
+// Create .click functions for each gem
+// When you click on it you activate a function addingNumber(pinkRandomNumber){}
+//takes the value of that random number and add it on to the userNumber
+//that value to userNumber + numberToAdd
+
+//Create IF statements for wining and losing
+// we can create a script for this part
+//If the randomNumber<userNumber (user loses)
+// we i++ to loses
+// ! we refresh the function of random number but not the score
+//else if (randomNumber===userNumber)(user wins)
+// we i++ to wins
+// ! we refresh the function of random number but not the score
